a2fbfec7f8183db489fdd95c6ff5fba4ecebbbb6
shekeleloy <202010637@gordoncollege.edu.ph>
2022-12-23 11:19:12 +0800

    admin dashboard
    done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Table from "../components/Table";
-
+import React from "react";
+import DashGrid from "../components/DashGrid";
 
 const Dashboard = () => {
   return (
-    <div className="dash-container">
-      <Table />
+    <div className="ad-dash">
+      <h1>Dashboard</h1>
+      <div className="ad-container">
+        <DashGrid />
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
---
+++ src/user/User_Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendartable from '../components/Calendartable';
+
+const UserCalendar = () => {
+    return (
+        <div>
+            <Calendartable />
+        </div>
+    );
+}
+
+export default UserCalendar;
