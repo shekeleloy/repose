a960358d8f24eae5709935f273e450aba6b6890a
shekeleloy <202010637@gordoncollege.edu.ph>
2022-12-24 02:20:50 +0800

    login user and admin
    done ready for test na po

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
-// Components
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
-
-// Pages
-import Calendar from "./pages/Calendar";
-import Dashboard from "./pages/Dashboard";
-import Leaves from "./pages/Leaves";
 import Login from "./pages/Login";
-// import ApplyLeave from "./user/ApplyLeave";
+
+
 
 const App = () => {
   return (
     <div className="App">
-    <Login />
-      <BrowserRouter>
-      <Navbar />
-        <Sidebar>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/leaves" element={<Leaves />} />
-          </Routes>
-        </Sidebar>
-      </BrowserRouter>
+      <Login />
     </div>
   );
 }
