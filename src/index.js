d270f90cfb73df2d0acf5508375648b678822ab6
shekeleloy <202010637@gordoncollege.edu.ph>
2023-01-14 17:47:13 +0800

    remove userapp in index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import UserApp from "./user/UserApp";
-import Login from "./pages/Login";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
